61df2b0145a7f9f5df6ae9c2528f93220c779730
abuishak <39011865+abuishak@users.noreply.github.com>
2020-06-21 15:47:23 -0500

    Add files via upload

---
+++ INFO1314.cs
@@ -0,0 +1,58 @@
+body { 
+    margin-left:auto;
+	margin-right:auto;
+    padding:0;
+    color:#000000;
+    background:#0000FF;
+    }
+header {
+	background-image:url(blue_banner.fw.jpg);
+		background-repeat:no-repeat;
+		}
+#wrapper{
+	width:960px;
+		  margin-right: auto;
+		   margin-left: auto;
+	background:#F2F2F2;
+	}
+#main{
+	background:000000;
+	}
+li{
+	display:list-item;
+	padding-bottom:5px;
+}
+#nav a:link { color: #ff0000; }
+#nav a:active { color: #00cc00; }
+#nav a:visited { color: #00cc00;}
+#nab a:hover {
+		text-decoration:none;
+		background-color: #00ff00;
+	}
+a:link,a:visited{
+	color:#0000FF;
+	text-decoration: underline;
+	cursor: auto;
+}
+
+#h1 {
+	color:#000000;
+	font-family:sans-serif; 
+	
+}
+#h2 {
+	color:#000000;
+	font-family:sans-serif; 
+	
+	
+}
+#h3 {
+	color:#000000;
+	font-family:sans-serif; 
+}
+
+
+#footer{
+	border-top:1px solid #003366;
+	font-size:.8em;  
+text-align:right;
